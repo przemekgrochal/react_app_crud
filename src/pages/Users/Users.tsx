386c6eb66770fb275be87347ca3075b8c9f07ce9
master <pprzemek144@gmail.com>
2020-09-13 17:17:54 +0200

    deleted user

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -3,12 +3,27 @@ import DataTable from 'react-data-table-component';
 import { useSelector, useDispatch } from 'react-redux';
 import { rootActions } from '../../redux/actions/index';
 import { IReduxState } from '../../dataTypes/IReduxState';
+import { Popup } from 'devextreme-react/popup';
+import { Button } from 'devextreme-react/button';
+import TextBox from 'devextreme-react/text-box';
+import RadioGroup from 'devextreme-react/radio-group';
 import Loader from 'react-loader-spinner';
 import jsonData from './data/data.json';
 import './style/Users.scss';
 
 const Users = () => {
     const [currentPage, setCurrentPage] = useState<number>(1);
+    const [popupVisible, setPopupVisible] = useState<any>(false);
+    const [dataInput, setDataInput] = useState<any>({
+        id: 0,
+        create: false,
+        update: false,
+        name: '',
+        surname: '',
+        email: '',
+        gender: '',
+        status: '',
+    });
     const urlApi: string = `${process.env.REACT_APP_API_URL}/users?page=${currentPage}`;
 
     const reduxState: any = useSelector((state: IReduxState) => {
@@ -42,13 +57,100 @@ const Users = () => {
         };
     }, [currentPage]);
 
+    const createUser = () => {
+        dispatch(
+            rootActions.usersActions.readUser({
+                urlApi: urlApi,
+                methods: 'POST',
+                headers: {
+                    ['Content-Type']: 'application/json',
+                    ['Authorization']: `Bearer ${process.env.REACT_APP_API_TOKEN}`,
+                },
+                bodyData: {
+                    name: dataInput.name + ' ' + dataInput.surname,
+                    email: dataInput.email,
+                    gender: dataInput.gender,
+                    status: dataInput.status,
+                },
+            })
+        );
+
+        setDataInput({
+            id: 0,
+            create: false,
+            update: false,
+            name: '',
+            surname: '',
+            email: '',
+            gender: '',
+            status: '',
+        });
+
+        setPopupVisible(false);
+    };
+
+    const deleteUser = (userId: any) => {
+        // setCurrentUser(userId);
+        console.log(userId);
+        console.log(`${process.env.REACT_APP_API_URL}/users/${userId}`);
+        dispatch(
+            rootActions.usersActions.readUser({
+                urlApi: `${process.env.REACT_APP_API_URL}/users/${userId}`,
+                methods: 'DELETE',
+                headers: {
+                    ['Content-Type']: 'application/json',
+                    ['Authorization']: `Bearer ${process.env.REACT_APP_API_TOKEN}`,
+                },
+                bodyData: null,
+            })
+        );
+
+        setDataInput({
+            id: 0,
+            create: false,
+            update: false,
+            name: '',
+            surname: '',
+            email: '',
+            gender: '',
+            status: '',
+        });
+    };
+
+    const handleChange = (e: any) => {
+        e.event.target.value = e.event.target.value.replace(/ /g, '');
+        setDataInput({
+            ...dataInput,
+            [e.event.target.name]: e.event.target.value,
+        });
+    };
+
+    const handleSubmit = () => {
+        if (dataInput.upadate) {
+            window.alert('user update');
+        }
+
+        if (dataInput.create) {
+            createUser();
+        }
+    };
+
     const columns = [
         {
-            name: 'Options',
-            cell: () => <button onClick={(e) => console.log(e)}>Action</button>,
-            // ignoreRowClick: true,
-            // allowOverflow: true,
-            // button: true,
+            name: 'Settings',
+            cell: (row: any, index: any, column: any, id: any) => {
+                return (
+                    <>
+                        <Button
+                            icon="trash"
+                            onClick={() => {
+                                deleteUser(row.id);
+                            }}
+                        />
+                        <Button icon="edit" onClick={(e) => console.log(row)} />
+                    </>
+                );
+            },
         },
         {
             name: 'Id', // Name column
@@ -88,7 +190,7 @@ const Users = () => {
     ];
 
     return (
-        <div>
+        <section className="users">
             <DataTable
                 title="Users"
                 columns={columns}
@@ -117,10 +219,20 @@ const Users = () => {
                 subHeaderComponent={
                     <div className="xdt-header-container">
                         <div
+                            className="btn-add-user"
+                            id="create-user"
                             style={{
                                 color: '#606060',
                                 fontSize: '34px',
                                 fontWeight: 'bold',
+                                cursor: 'pointer',
+                            }}
+                            onClick={() => {
+                                setDataInput({
+                                    ...dataInput,
+                                    create: true,
+                                });
+                                setPopupVisible(true);
                             }}
                         >
                             +
@@ -128,7 +240,81 @@ const Users = () => {
                     </div>
                 }
             />
-        </div>
+            <Popup
+                visible={popupVisible}
+                onHiding={() => setPopupVisible(false)}
+                // dragEnabled={false}
+                closeOnOutsideClick={true}
+                showTitle={true}
+                title="User"
+                width={400}
+                height={450}
+            >
+                <p>
+                    <TextBox
+                        name="name"
+                        mode="text"
+                        placeholder="Enter name"
+                        showClearButton={true}
+                        onChange={handleChange}
+                        value={dataInput.name}
+                    />
+                </p>
+                <p>
+                    <TextBox
+                        name="surname"
+                        mode="text"
+                        placeholder="Enter surname"
+                        showClearButton={true}
+                        onChange={handleChange}
+                        value={dataInput.surname}
+                    />
+                </p>
+                <p>
+                    <TextBox
+                        name="email"
+                        mode="text"
+                        placeholder="Enter e-mail address"
+                        showClearButton={true}
+                        onChange={handleChange}
+                        value={dataInput.email}
+                    />
+                </p>
+                <p>
+                    <RadioGroup
+                        name="gender"
+                        items={['Male', 'Female']}
+                        onValueChanged={(e) =>
+                            setDataInput({
+                                ...dataInput,
+                                gender: e.value,
+                            })
+                        }
+                        value={dataInput.gender}
+                    />
+                    <hr />
+                    <RadioGroup
+                        items={['Active', 'Inactive']}
+                        onValueChanged={(e) =>
+                            setDataInput({
+                                ...dataInput,
+                                status: e.value,
+                            })
+                        }
+                        value={dataInput.status}
+                    />
+                </p>
+                <p>
+                    <Button
+                        width={120}
+                        text="Send"
+                        type="success"
+                        stylingMode="contained"
+                        onClick={handleSubmit}
+                    />
+                </p>
+            </Popup>
+        </section>
     );
 };
 
